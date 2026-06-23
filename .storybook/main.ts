16b4abb2b1cbdbe13b3ad13fbba0fc334ce17f8e
badjoke-lab <badjoke.lab@gmail.com>
2026-06-23 17:49:31 +0900

    ci: use bounded Storybook test build

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,7 +7,13 @@ const config: StorybookConfig = {
     name: "@storybook/nextjs-vite",
     options: {},
   },
+  core: {
+    disableTelemetry: true,
+  },
   staticDirs: ["../public"],
+  typescript: {
+    reactDocgen: false,
+  },
 };
 
 export default config;
