95d4f14bb9ad953e960513ae281f1a51af7104f1
badjoke-lab <badjoke.lab@gmail.com>
2026-06-26 15:54:43 +0900

    feat: start Xaman application inspection

---
+++ scripts/mainnet-xaman-attestation-application.mjs
@@ -0,0 +1,2 @@
+import { readCallbackDetails, readGitHubContext, requireText } from "./mainnet-xaman-attestation-context.mjs";
+import { readEnabledApplication, requestXamanJson, xamanAuthHeaders } from "./mainnet-xaman-attestation-http.mjs";
